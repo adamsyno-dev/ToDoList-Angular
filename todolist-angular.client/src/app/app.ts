3a288e3674449cec3f7f71ffe978edba31942c6e
Adam Synoradzki <adam@methsofapp.onmicrosoft.com>
2026-05-13 18:27:30 +1000

    Stable app

--- todolist-angular.client/src/app/app.ts
+++ todolist-angular.client/src/app/app.ts
@@ -1,5 +1,13 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { retry, delay } from 'rxjs/operators';
+
+export interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF?: number;
+  summary?: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -16,16 +24,34 @@ export class App implements OnInit {
     this.getForecasts();
   }
 
+  //getForecasts() {
+  //  this.http.get<WeatherForecast[]>('/weatherforecast').subscribe({
+  //    next: (result) => {
+  //      this.forecasts = result;
+  //    },
+  //    error: (error) => {
+  //      console.error(error);
+  //    }
+  //  });
+  //}
+
   getForecasts() {
-    this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-      (result) => {
-        this.forecasts = result;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+    this.http.get<WeatherForecast[]>('/weatherforecast')
+      .pipe(
+        // If the server returns a 500 because it's still starting, 
+        // wait 2 seconds and try again. Do this up to 3 times.
+        retry({ count: 3, delay: 2000 })
+      )
+      .subscribe({
+        next: (result) => {
+          this.forecasts = result;
+        },
+        error: (error) => {
+          console.error("Data failed to load after retries:", error);
+        }
+      });
   }
+ 
 
   protected readonly title = signal('todolist-angular.client');
 }
